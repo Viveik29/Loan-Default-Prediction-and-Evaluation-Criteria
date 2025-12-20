8114814f1ac4f01a31b20e964ade77910a0f871f
Viveik29 <viveik29@gmail.com>
2025-12-20 18:09:26 +0530

    Update aws_secrte .env file

--- src/Data_ingestion.py
+++ src/Data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 import os
 import pandas as pd
 import numpy as np
+from dotenv import load_dotenv
 # Load parameters from params.yaml
 def params_load(file_path):
     with open(file_path, "r") as f:
@@ -24,15 +25,17 @@ def main():
     bucket_name = params["data_ingestion"]["bucket_name"]
     file_name = params["data_ingestion"]["file_name"]
     folder_path = params['data_ingestion']['folder_path']
-    aws_access_key = params["data_ingestion"]["aws_access_key_id"]
-    aws_secrets = params["data_ingestion"]["aws_secret_access_key"]
+    # aws_access_key = params["data_ingestion"]["aws_access_key_id"]
+    # aws_secrets = params["data_ingestion"]["aws_secret_access_key"]
     test_size = params["data_ingestion"]["test_size"]
     local_save_path = "RAW_DATA/Downloaded_Data/Dataset.csv"
     # version_id = params["data_ingestion"]["version_id"]
     # output_path = params["data_ingestion"]["output_path"]
 
-
-    s3_obj = S3_connection(bucket_name,aws_access_key,aws_secrets)
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
+    s3_obj = S3_connection(bucket_name,AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
     s3_obj.fetch_data(folder_path,file_name)
     s3_obj.download_file(folder_path, file_name, local_save_path)
     #df= data_balancing(df)
